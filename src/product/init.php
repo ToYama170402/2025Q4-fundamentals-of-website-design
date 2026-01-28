c7e50239cf3b3cfefa05eab609c43ee063e0af83
ToYama170402 <yamato170402@gmail.com>
2026-01-28 14:47:23 +0900

    DB接続処理の実装

---
+++ src/product/init.php
@@ -0,0 +1,18 @@
+<?php
+function db(): PDO
+{
+  $mysql_user = getEnv("MYSQL_USER");
+  $mysql_password = getEnv("MYSQL_PASSWORD");
+  $dsn = "mysql:host=db;dbname=mydb;charset=utf8mb4";
+  $options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+  ];
+
+  try {
+    return new PDO($dsn, $mysql_user, $mysql_password, $options);
+  } catch (PDOException $e) {
+    exit("DB接続に失敗しました：" . $e->getMessage());
+  }
+}
